80c223c7d145f13c90427929294b9f59ab78c99c
wiizz <66723882+wiizzdev@users.noreply.github.com>
2021-12-18 23:42:19 +0100

    Add files via upload

---
+++ w_ammu/w_ammu.sql
@@ -0,0 +1,9 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+('clip','Chargeur', 2)
+;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('kevlar', 'Gilet Pare-Balles', 20)
+;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES ('weapon', 'PPA');
