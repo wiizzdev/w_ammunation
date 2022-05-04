9e1b5ad186d571f6ef3ef243b1041bb69377161a
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-05-04 16:15:34 +0200

    Delete w_ammu directory

--- w_ammu/w_ammu.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `items` (name, label) VALUES
-('clip', 'Chargeur');
-
-INSERT INTO `items` (`name`, `label`) VALUES
-('kevlar', 'Gilet Pare-Balles');
-
-INSERT INTO `licenses` (`type`, `label`) VALUES ('weapon', 'PPA');
